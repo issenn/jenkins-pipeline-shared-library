830d2e75926124b62c41a07c0b1680f28c809574
issenn <issenn@issenn.com>
2019-04-23 07:16:19 +0800

    Initial commit

--- vars/withHomebrew.groovy
+++ vars/withHomebrew.groovy
@@ -6,10 +6,21 @@ def call(method=null, Closure body={}) {
 
     print "Setting up Homebrew!"
 
+
+    def sout = new StringBuilder(), serr = new StringBuilder()
+    // def out = new StringBuffer()
+    // def err = new StringBuffer()
+
+    def command = "command -v ${metarunner}"
+    Process process = command.execute()
+    process.consumeProcessOutput(sout, serr)
+    // process.waitFor()
+    process.waitForOrKill(1000)
+    println process.text
+    if( sout.size() > 0 ) println sout.toString()
+    if( serr.size() > 0 ) println serr.toString()
+
     if (!"command -v ${metarunner}".execute().text.trim()) {
-        sh "env"
-        sh "command -v brew"
-        sh "which brew"
         print("command -v ${metarunner}".execute().text.trim())
         installHomebrew(metarunner)
     }
