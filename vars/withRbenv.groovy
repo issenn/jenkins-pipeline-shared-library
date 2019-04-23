6d7d3d0e1ccce0043280130631dcbe173d5db65d
issenn <issenn@issenn.com>
2019-04-23 15:00:18 +0800

    Initial commit

--- vars/withRbenv.groovy
+++ vars/withRbenv.groovy
@@ -22,6 +22,7 @@ def call(String version='2.5.1', String method=null, Closure cl) {
         installRbenv(metarunner)
     }
 
+    /*
     while (!fileExists("$HOME/.${metarunner}/versions/${version}/")) {
         try {
             utils.installVersion(metarunner, version,
