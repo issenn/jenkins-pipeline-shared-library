07778e7794b4aa9b38acf0b904414eacbdeb4c48
issenn <issenn@issenn.com>
2019-04-23 17:08:20 +0800

    Initial commit

--- vars/withRbenv.groovy
+++ vars/withRbenv.groovy
@@ -4,7 +4,7 @@ import io.issenn.jenkins.utils.utils
 
 
 // def call(String version='2.5.1', String method=null, Closure cl={}) {
-def call(Map parameters = [:], Closure body) {
+def call(Map parameters = [:], Closure body={}) {
 
     String version = parameters.get('version', '2.5.1')
     String method = parameters.get('method', 'keep')
